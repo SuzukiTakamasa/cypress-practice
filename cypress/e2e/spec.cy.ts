78d676e1ba620fe8b0e7c7c21560bca5dcc5d16f
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-08-20 01:45:58 +0900

    add papaparse for parsing test data of csv files

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,11 @@
+const Papa = require('papaparse');
+
 describe('template spec', () => {
+let testCase = [];
+
+before(() => {
+})
+
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
